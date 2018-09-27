6e4e4a6384be2d24feab0c63569fbaef9dd69622
matthew17754 <matmoradi17@gmail.com>
2018-09-26 17:03:01 -0700

    Initial Commit
    The start of my quality work...

---
+++ AutoCTRL+S/SaveManager.Designer.cs
@@ -0,0 +1,172 @@
+﻿namespace AutoCTRL_S
+{
+    partial class SaveManager
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(SaveManager));
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.chkStartUp = new System.Windows.Forms.CheckBox();
+            this.chkNotification = new System.Windows.Forms.CheckBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(147, 12);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(62, 20);
+            this.numericUpDown1.TabIndex = 0;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            2,
+            0,
+            0,
+            0});
+            this.numericUpDown1.ValueChanged += new System.EventHandler(this.numericUpDown1_ValueChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(135, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Minute Invervals Per Save:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(43, 162);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(130, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Moradi Software Solutions";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            // 
+            // chkStartUp
+            // 
+            this.chkStartUp.AutoSize = true;
+            this.chkStartUp.Location = new System.Drawing.Point(15, 50);
+            this.chkStartUp.Name = "chkStartUp";
+            this.chkStartUp.Size = new System.Drawing.Size(168, 17);
+            this.chkStartUp.TabIndex = 3;
+            this.chkStartUp.Text = "Launch Application On Starup";
+            this.chkStartUp.UseVisualStyleBackColor = true;
+            this.chkStartUp.CheckedChanged += new System.EventHandler(this.chkStartUp_CheckedChanged);
+            // 
+            // chkNotification
+            // 
+            this.chkNotification.AutoSize = true;
+            this.chkNotification.Location = new System.Drawing.Point(15, 73);
+            this.chkNotification.Name = "chkNotification";
+            this.chkNotification.Size = new System.Drawing.Size(181, 17);
+            this.chkNotification.TabIndex = 4;
+            this.chkNotification.Text = "Play Sound Notification On Save";
+            this.chkNotification.UseVisualStyleBackColor = true;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.pictureBox1.Image = global::AutoCTRL_S.Properties.Resources.GitHub1;
+            this.pictureBox1.Location = new System.Drawing.Point(36, 125);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(37, 26);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 5;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 96);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(197, 23);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "OK";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(79, 130);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(107, 15);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "View On GitHub";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // SaveManager
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(221, 184);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.chkNotification);
+            this.Controls.Add(this.chkStartUp);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.numericUpDown1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "SaveManager";
+            this.ShowInTaskbar = false;
+            this.Text = "AutoCTRL+S Settings";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.SaveManager_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.CheckBox chkStartUp;
+        private System.Windows.Forms.CheckBox chkNotification;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ AutoCTRL+S/SaveManager.cs
@@ -0,0 +1,111 @@
+﻿using AutoCTRL_S.Properties;
+using Microsoft.Win32;
+using System;
+using System.Diagnostics;
+using System.Timers;
+using System.Windows.Forms;
+
+namespace AutoCTRL_S
+{
+    public partial class SaveManager : Form
+    {
+        private NotifyIcon trayIcon;
+        public System.Timers.Timer saveTimer;
+
+        public SaveManager()
+        {
+            InitializeComponent();
+            startSaveTimer();
+
+            // Initialize Tray Icon
+            trayIcon = new NotifyIcon()
+            {
+                Icon = Resources.SaveMe,
+                ContextMenu = new ContextMenu(new MenuItem[] {
+                new MenuItem("Settings", settings), new MenuItem("Exit", exit)
+            }),
+                Visible = true
+            };
+        }
+
+        void exit(object sender, EventArgs e)
+        {
+            // Hide tray icon, otherwise it will remain shown until user mouses over it
+            trayIcon.Visible = false;
+            Environment.Exit(0);
+        }
+
+        void settings(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Normal;
+            saveTimer.Stop();
+        }
+
+    private void startSaveTimer()
+        {
+            saveTimer = new System.Timers.Timer((double)numericUpDown1.Value * 60000);
+            saveTimer.Elapsed += saveTimer_Elapsed;
+            saveTimer.Start();
+        }
+
+        private void saveTimer_Elapsed(object sender, ElapsedEventArgs e)
+        {
+            SendKeys.SendWait("^(s)");
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Minimized;
+            this.ShowInTaskbar = false;
+        }
+
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+            Process.Start("https://github.com/matthew17754/AutoCTRL-S");
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            Process.Start("https://github.com/matthew17754/AutoCTRL-S");
+        }
+
+        private void SaveManager_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            this.WindowState = FormWindowState.Minimized;
+            e.Cancel = true;
+            startSaveTimer();
+        }
+
+        private void SetStartup()
+        {
+            RegistryKey rk = Registry.CurrentUser.OpenSubKey
+                ("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run", true);
+
+            if (chkStartUp.Checked)
+                rk.SetValue("AutoCTRL+S", Application.ExecutablePath);
+            else
+                rk.DeleteValue("AutoCTRL+S", false);
+        }
+
+        private void chkStartUp_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Properties.Settings.Default["MinInterval"] = numericUpDown1.Value;
+            Properties.Settings.Default.Save();
+            Properties.Settings.Default["Startup"] = chkStartUp.Checked;
+            Properties.Settings.Default.Save();
+            Properties.Settings.Default["Notification"] = chkNotification.Checked;
+            Properties.Settings.Default.Save(); // Saves settings in application configuration file
+            startSaveTimer();
+            this.WindowState = FormWindowState.Minimized;
+        }
+    }
+}
